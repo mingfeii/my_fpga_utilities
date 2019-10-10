41b4a7eb1390e8a41cceffffad98c6b9dcc27b89
mingfeii <mingfeii@outlook.com>
2019-10-10 10:04:50 +0800

    up

---
+++ ram/ram_dp_sr_sw.v
@@ -0,0 +1,63 @@
+//双端口ram，同步读写
+module ram_dp_sr_sw #(
+    parameter data_0_WIDTH = 8,
+    parameter ADDR_WIDTH = 8,
+    parameter RAM_DEPTH = 1 << ADDR_WIDTH
+)(
+    input clk,
+
+    input [ADDR_WIDTH-1:0] address_0,
+    input we_0,
+    input oe_0,
+    input [ADDR_WIDTH-1:0] address_1,
+    input we_1,
+    input oe_1,
+    inout [data_0_WIDTH-1:0] data_0 ;
+    inout [data_0_WIDTH-1:0] data_1 ;
+
+     //--------------Code Starts Here------------------ 
+// Memory Write Block 
+// Write Operation : When we_0 = 1, cs_0 = 1
+always @ (posedge clk)
+begin : MEM_WRITE
+  if ( cs_0 && we_0 ) begin
+     mem[address_0] <= data_0;
+  end else if (cs_1 && we_1) begin 
+     mem[address_1] <= data_1;
+  end
+end
+
+  
+
+// Tri-State Buffer control 
+// output : When we_0 = 0, oe_0 = 1, cs_0 = 1
+assign data_0 = (cs_0 && oe_0 &&  ! we_0) ? data_0_out : 8'bz; 
+
+// Memory Read Block 
+// Read Operation : When we_0 = 0, oe_0 = 1, cs_0 = 1
+always @ (posedge clk)
+begin : MEM_READ_0
+  if (cs_0 &&  ! we_0 && oe_0) begin
+    data_0_out <= mem[address_0]; 
+  end else begin
+    data_0_out <= 0; 
+  end  
+end 
+
+//Second Port of RAM
+// Tri-State Buffer control 
+// output : When we_0 = 0, oe_0 = 1, cs_0 = 1
+assign data_1 = (cs_1 && oe_1 &&  ! we_1) ? data_1_out : 8'bz; 
+// Memory Read Block 1 
+// Read Operation : When we_1 = 0, oe_1 = 1, cs_1 = 1
+always @ (posedge clk)
+begin : MEM_READ_1
+  if (cs_1 &&  ! we_1 && oe_1) begin
+    data_1_out <= mem[address_1]; 
+  end else begin
+    data_1_out <= 0;
+  end
+end
+
+endmodule // End of Module ram_dp_sr_sw
+)
