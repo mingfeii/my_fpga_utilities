c9cf29e50b31a905d00c60cf1077e0b40bc7418f
mingfeii <mingfeii@outlook.com>
2019-10-22 20:23:38 +0800

    up

--- KeyDebounce/proj/key_debounce.runs/impl_1/key_jitter.tcl
+++ KeyDebounce/proj/key_debounce.runs/impl_1/key_jitter.tcl
@@ -65,6 +65,7 @@ start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
+  set_param xicom.use_bs_reader 1
   open_checkpoint key_jitter_routed.dcp
   set_property webtalk.parent_dir C:/Users/mingf/Desktop/github/my_fpga_utilities/KeyDebounce/proj/key_debounce.cache/wt [current_project]
   catch { write_mem_info -force key_jitter.mmi }
